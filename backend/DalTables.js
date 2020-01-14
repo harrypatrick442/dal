8d1fb425435fad95d3f226538f93fe8e835f84ec
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-01-14 19:02:01 +0000

    debugginggggggg

--- backend/DalTables.js
+++ backend/DalTables.js
@@ -3,7 +3,6 @@ module.exports= new (function(){
 	this.createTable = function(databaseConfiguration, table){
 		var dal = new Dal(databaseConfiguration);
 		console.log(table.getCreate());
-		return new Promise((resolve, rejecct)=>{});
 		return dal.raw(table.getCreate());
 	};
 })();

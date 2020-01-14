66ed69e99b140fdae0b872e42527cc59d891042b
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-01-14 10:44:44 +0000

    shard builder in dal now

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const Dal = require('./backend/Dal');
-Object.defineProperty(Dal, 'getIntsTable', {
-  get: function(){return require('./backend/getIntsTable')}
-});
-Object.defineProperty(Dal, 'DalDatabases', {
-  get: function(){return require('./backend/DalDatabases')}
-});
-Object.defineProperty(Dal, 'Mssql', {
-  get: function(){return require('./backend/Mssql')}
-});
-Object.defineProperty(Dal, 'Sqlite', {
-  get: function(){return require('./backend/Sqlite')}
+var classNames =['getIntsTable', 'DalDatabases', 'Mssql', 'Sqlite', 'ShardBuilder'];
+classNames.forEach((className)=>{
+	Object.defineProperty(Dal, className, {
+	  get: function(){return require('./'+className+'/')}
+	});
 });
 module.exports= Dal;

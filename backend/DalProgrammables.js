a4402ad5341144023c1bea8fdada9a8896948259
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-01-14 12:01:30 +0000

    debugging

--- backend/DalProgrammables.js
+++
@@ -1,7 +0,0 @@
-module.exports= new(function(configuration){
-	const Dal =require('./Dal');
-	const dal = new Dal(configuration);
-	this.updateProgrammable = function(programmable){
-		return dal.raw(programmable.getCreateOrAlterDefinition());
-	};
-})();

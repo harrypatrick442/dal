77e65592ca8760d9863010b5f7a19d0e89e0d897
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-01-14 18:44:28 +0000

    debugging

--- backend/DalTables.js
+++ backend/DalTables.js
@@ -1,7 +1,7 @@
-module.exports= function(configuration){
+module.exports= new (function(configuration){
 	const Dal =require('./Dal');
 	const dal = new Dal(configuration);
 	this.createTable = function(table){
 		return dal.raw(table.getCreate());
 	};
-};
+})();

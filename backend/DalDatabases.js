1b87113830171fe5623497dd313315c10f8ed80a
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-01-14 09:58:33 +0000

    debugging

--- backend/DalDatabases.js
+++ backend/DalDatabases.js
@@ -3,12 +3,20 @@ module.exports= new(function(configuration){
 	const DatabaseConfiguration = require('configuration').DatabaseConfiguration;
 	this.createDatabase = function(currentDatabaseConfiguration, name){
 		return new Promise((resolve, reject)=>{
-			var statement = "If(db_id(N'"+name+"') IS NULL)CREATE DATABASE "+name+';';
-			var dal = new Dal(currentDatabaseConfiguration);
 			dal.raw(statement).then(()=>{
+				var statement = "If(db_id(N'"+name+"') IS NULL)CREATE DATABASE "+name+';';
+				var dal = new Dal(currentDatabaseConfiguration);
 				resolve(new DatabaseConfiguration({user:currentDatabaseConfiguration.getUser(), password:currentDatabaseConfiguration.getPassword(),
 				server:currentDatabaseConfiguration.getServer(), database:name}));
 			}).catch(reject);
 		});
+	};
+	
+	this.createDatabase = function(currentDatabaseConfiguration, name){
+		return new Promise((resolve, reject)=>{
+			var dal = new Dal(currentDatabaseConfiguration);
+			var statement = "If(db_id(N'"+name+"') IS NOT NULL)DROP DATABASE "+name+';';
+			dal.raw(statement).then(resolve).catch(reject);
+		}).catch(reject);
 	};
 })();
